ed407cc0ec18c1bf246de2b02963550c73fca5f9
Jasper85125 <Jaspervanderlaan1105@gmail.com>
2026-09-18 11:44:55 +0200

    add the save file it is in save folder

---
+++ Assets/Scrips/GameData.cs
@@ -0,0 +1,9 @@
+using System;
+
+[Serializable]
+public class GameData
+{
+    public int highestLevelUnlocked = 1;
+    public int totalDeaths = 0;
+    public int totalJumps = 0;
+}
--- Assets/Scrips/LevelSelector.cs
+++ Assets/Scrips/LevelSelector.cs
@@ -1,18 +1,40 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LevelSelector : MonoBehaviour
 {
     public int level;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Button button;
+
+    private void Start()
     {
-        
+        button = GetComponent<Button>();
+
+        if (GameManager.Instance != null && GameManager.Instance.Data != null)
+        {
+            bool isUnlocked = level <= GameManager.Instance.Data.highestLevelUnlocked;
+
+            // Gray out and disable UI button if locked
+            if (button != null)
+            {
+                button.interactable = isUnlocked;
+            }
+        }
     }
 
     public void OpenScene()
     {
+        // Guard check to prevent loading locked levels
+        if (GameManager.Instance != null && GameManager.Instance.Data != null)
+        {
+            if (level > GameManager.Instance.Data.highestLevelUnlocked)
+            {
+                Debug.LogWarning($"Level {level} is locked!");
+                return;
+            }
+        }
+
         SceneManager.LoadScene("Level " + level.ToString());
     }
 }
