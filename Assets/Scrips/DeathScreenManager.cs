abb65541d8474d15797faf13f69c96cea24a9503
Jasper85125 <Jaspervanderlaan1105@gmail.com>
2026-09-21 12:23:48 +0200

    replay on death

--- Assets/Scrips/DeathScreenManager.cs
+++ Assets/Scrips/DeathScreenManager.cs
@@ -7,7 +7,27 @@ namespace Scrips
     {
         [Header("Scene Names or Indexes")]
         [SerializeField] private string mainMenuSceneName = "Main Menu";
-        
+
+        public static string lastLevelName;
+
+        // Ensure this method is marked public static
+        public static void RecordCurrentLevel()
+        {
+            lastLevelName = SceneManager.GetActiveScene().name;
+        }
+
+        public void ReplayLevel()
+        {
+            if (!string.IsNullOrEmpty(lastLevelName))
+            {
+                SceneManager.LoadScene(lastLevelName);
+            }
+            else
+            {
+                Debug.LogWarning("No last level saved!");
+            }
+        }
+
         public void LoadMainMenu()
         {
             SceneManager.LoadScene(mainMenuSceneName);
